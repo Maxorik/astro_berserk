7510af4c91262cdbee2ba9587a838dd1c77306df
Maxorik <gfifful@gmail.com>
2026-09-25 22:49:07 +0700

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
-
 	outDir: './docs/',
-
 	fonts: [
 		{
 			provider: fontProviders.local(),
