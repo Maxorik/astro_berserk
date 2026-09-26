b018d54b11d50b784c279bb671b8d17361f4dc19
Maxorik <gfifful@gmail.com>
2026-09-26 19:09:40 +0700

    change header

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Velum Title';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Журнал "Берсерк"';
+export const SITE_DESCRIPTION = 'Агрегатор статей по ККИ "Берсерк"';
